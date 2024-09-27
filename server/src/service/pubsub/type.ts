d886fdf346d02d06249db7bfc873e0c26b0d682f
subhamnayak76 <subhamnayak9638@gmail.com>
2024-09-27 21:25:30 +0530

    a

--- server/src/service/pubsub/type.ts
+++ server/src/service/pubsub/type.ts
@@ -1,4 +1,4 @@
 export interface PubSubService {
     publish(topicId: string, payload: Record<string, unknown>): Promise<string>;
-    validatePayload(payload: Record<string, any>): boolean;
+    
   }
